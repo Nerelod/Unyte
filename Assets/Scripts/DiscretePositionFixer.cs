c63d340208682bdfbd5bfc2e7e5a817dde65b681
Nerelod <dapperfellowdaniel@gmail.com>
2019-05-13 14:17:59 -0600

    achieving pixel perfectness

---
+++ Assets/Scripts/DiscretePositionFixer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscretePositionFixer : MonoBehaviour {
+    public float pixPerUnit = 32;
+    void LateUpdate() {
+        transform.position = new Vector3(
+            Mathf.Round(transform.parent.position.x * pixPerUnit) / pixPerUnit,
+            Mathf.Round(transform.parent.position.y * pixPerUnit) / pixPerUnit,
+            transform.parent.position.z);
+    }
+}
