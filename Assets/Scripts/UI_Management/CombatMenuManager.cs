5b4d4e3759faa9492878248835d18d643cd87828
Nerelod <dapperfellowdaniel@gmail.com>
2019-06-21 19:49:50 -0400

    Added health potion object and fixed item select in combat. TODO: fix picking up items when menu is up

--- Assets/Scripts/UI_Management/CombatMenuManager.cs
+++ Assets/Scripts/UI_Management/CombatMenuManager.cs
@@ -13,17 +13,29 @@ public class CombatMenuManager : MonoBehaviour
     public Button itemReturnButton;
     public Button healthPotionButton;
 
+    public Text healthPotionText;
+
     void Start()
     {
         abilitySelectPanel.SetActive(false);
         itemSelectPanel.SetActive(false);
     }
 
+    private void checkItemInCombat(string itemName, Button button, Text textBox) {
+        if (DataManager.playerOne.itemManager.aquiredItems.Contains(itemName)) {
+            button.gameObject.SetActive(true);
+            textBox.text = itemName + " " + DataManager.playerOne.itemManager.getAmountOfItem(itemName);
+        }
+        else {
+            button.gameObject.SetActive(false);
+        }
+    }
+
     public void whenTurnedOn(){
         itemSelectPanel.SetActive(true);
         itemReturnButton.Select();
         // activate or deactivate health potion button if there ar/aren't health potions
-        if(DataManager.playerOne.itemManager.aquiredItems.Contains("Health_Potion")){ healthPotionButton.gameObject.SetActive(true); } else { healthPotionButton.gameObject.SetActive(false); }
+        checkItemInCombat("Health Potion", healthPotionButton, healthPotionText);
     }
 
     void Update()
