90c79972045c2e739ef0198b0f5782876083720b
Nerelod <dapperfellowdaniel@gmail.com>
2020-05-04 16:48:32 -0400

    added tiles, changed village scene, and added LayerController

---
+++ Assets/Scripts/Controllers/LayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerController : MonoBehaviour
+{
+    [SerializeField] private PlayerController player;
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private int layerOffset;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (transform.position.y < player.transform.position.y) { // if below
+            sprite.sortingOrder = player.render.sortingOrder + layerOffset;
+        }
+        else { // if above
+            sprite.sortingOrder = player.render.sortingOrder - layerOffset;
+        }
+    }
+}
